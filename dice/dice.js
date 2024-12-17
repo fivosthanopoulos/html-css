a0a351a95ef59165feb90fbf42a495ffbe33c17b
Fivos Thanopoulos <fivosthanopoulos@msn.com>
2024-12-17 18:34:29 +0200

    Add files via upload

---
+++ dice/dice.js
@@ -0,0 +1,14 @@
+document.getElementById("btn").addEventListener("click", function() {
+
+let dice1 = Math.floor(Math.random() * 6 ) + 1;
+let dice2 = Math.floor(Math.random() * 6 ) + 1;
+
+ document.getElementById("dice1").textContent = dice1;
+ document.getElementById("dice2").textContent = dice2;
+
+
+
+
+
+})
+
